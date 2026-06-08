dd4e7a984d4c9d0e2711c7e22734cc234665c003
cylina ho <hocylina@gmail.com>
2026-06-08 23:05:25 +0800

    change the ontrigger to collision cause my model when transparent when i turn on the is trigger

--- Assets/Scripts/Bed.cs
+++ Assets/Scripts/Bed.cs
@@ -5,17 +5,17 @@ public class Bed : MonoBehaviour
 // How much HP the player loses when they interact with this bed
     public int hpDamage = 10;
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision collision)
     {
         // Check if the thing that touched the bed has a Player script on it
-        Player playerScript = other.GetComponent<Player>();
+        Player playerScript = collision.gameObject.GetComponent<Player>();
 
         if (playerScript != null)
         {
             // Subtract HP directly from the player
             playerScript.currentHP -= hpDamage;
             
-            Debug.Log("🛌 Ouch! You touched the uncomfortable bed. Current HP: " + playerScript.currentHP);
+            Debug.Log("Ouch! You touched the uncomfortable bed. Current HP: " + playerScript.currentHP);
 
             if (playerScript.currentHP <= 0)
             {
