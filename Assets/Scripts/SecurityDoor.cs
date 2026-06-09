23536a1581e05a36f4ff568568d0cf491e9619e3
cylina ho <hocylina@gmail.com>
2026-06-09 21:19:32 +0800

    fixing my security door

--- Assets/Scripts/SecurityDoor.cs
+++ Assets/Scripts/SecurityDoor.cs
@@ -3,15 +3,13 @@ using UnityEngine;
 public class SecurityDoor : MonoBehaviour
 {
     [Header("Door Settings")]
-    public Vector3 rotateAmount = new Vector3(0, 90, 0); // Rotation when opening
-    public int requiredCards = 10; // Number of cards needed to open
+    public Vector3 rotateAmount = new Vector3(0, 90, 0); 
+    public int requiredCards = 10; 
 
     private bool isOpen = false;
 
-    // This is the method the Player script will call
     public void Interact(Player player)
     {
-        // If the door is already open, just close it like a normal door
         if (isOpen)
         {
             transform.Rotate(-rotateAmount);
@@ -20,9 +18,8 @@ public class SecurityDoor : MonoBehaviour
             return;
         }
 
-        // If it's closed, check if the player has enough score/cards
-        // Note: We access the player's 'score' here. 
-        if (player.CardID() >= requiredCards)
+        // FIXED: Changed player.CardID() to player.GetScore() to match your Player script
+        if (player.GetScore() >= requiredCards)
         {
             transform.Rotate(rotateAmount);
             isOpen = true;
@@ -30,7 +27,7 @@ public class SecurityDoor : MonoBehaviour
         }
         else
         {
-            int missingCards = requiredCards - player.CardID();
+            int missingCards = requiredCards - player.GetScore();
             print($"🔒 Access Denied! You need {missingCards} more security cards.");
         }
     }
