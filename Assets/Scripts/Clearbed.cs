1d6d40a89410f643ad53e8d07983d785e5dec1e3
cylina ho <hocylina@gmail.com>
2026-06-09 17:38:06 +0800

    adding sound to my game

--- Assets/Scripts/Clearbed.cs
+++ Assets/Scripts/Clearbed.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Clearbed : MonoBehaviour
 {
-void Start()
+    private void OnCollisionEnter(Collision collision)
     {
+        // This will print a message in your Console log the exact millisecond 
+        // ANYTHING physically touches the bed.
+        Debug.Log("Something physically hit the bed: " + collision.gameObject.name);
+
         AudioSource audioSource = GetComponent<AudioSource>();
-        if (audioSource != null) 
+        if (audioSource == null) 
         {
             audioSource.Play();
         }
