167d5d00dd1dbe541ffe1686ebb926b8f702908d
cylina ho <hocylina@gmail.com>
2026-06-09 20:53:46 +0800

    cap

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,7 +4,7 @@ public class Door : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
-    public Vector3 rotateAmount = New Vector3(0, 90, 0); // Amount to rotate when opening the door
+    public Vector3 rotateAmount = new Vector3(0, 90, 0); // Amount to rotate when opening the door
 
     bool isOpen = false; // Whether the door is open or closed
 
